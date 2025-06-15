c2e5630bcd345ed6c6178c4499c7625ceeac9d04
Muhammad Haekal Al Ghiffari <143934598+Haekalgtg@users.noreply.github.com>
2025-06-15 22:46:11 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,4 @@
 <?php
-$host = "localhost";     
-$user = "root";         
-$pass = "";              
-$db   = "db_catering";    
-
-$koneksi = new mysqli($host, $user, $pass, $db);
-
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
-}
+$koneksi = new mysqli("sql110.infinityfree.com", "if0_39236930", "T9GazsQgsvDaKbT", "if0_39236930_db_catering");
+if ($koneksi->connect_error) die("Koneksi gagal: " . $koneksi->connect_error);
 ?>
